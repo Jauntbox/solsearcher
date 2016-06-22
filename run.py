0c04b8f975dd9b8ece3f54be5308da5666128aaa
Kevin Moore <jauntbox@gmail.com>
2016-06-21 17:29:23 -0700

    First working version. Refactored javascript and post functions so that intermediate files are not saved to the server. Previous version had race condition problems with mutiple gunicorn threads.

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 from solsearcher import app
-app.run('0.0.0.0', debug=True)
+#import logging
+
+#logHandler = logging.FileHandler('/Users/Kevin/Dropbox/Insight_SV2016B/solsearcher_flask_aws/flask.log')
+#logHandler.setLevel(logging.DEBUG)
+#app.logger.addHandler(logHandler)
+#app.logger.setLevel(logging.DEBUG)
+
+#if __name__ == '__main__':
+app.run(host='0.0.0.0')
--- solsearcher/views.py
+++ solsearcher/views.py
@@ -19,7 +19,6 @@ import subprocess
 import uuid
 
 from solarize import split_images, dbscan_features, dbscan_features_new
-from api_keys import google_maps_api_key
 #TODO: Hook in code from solarize.py to analyze downloaded image and add a rectangle to the map
 
 #user = database_settings.username #add your username here (same as previous postgreSQL) 
@@ -40,7 +39,7 @@ def acquire_image(lat, long):
 	    'zoom': '20',
 	    'size': '640x400',
 	    'scale': '2',
-	    'key': google_maps_api_key
+	    'key': 'AIzaSyAGQnc_d6-AQimy4dkGWn_GhwAQrJkdmrI'
 	}
 	req = requests.get('https://maps.googleapis.com/maps/api/staticmap?',params=my_params)
 	print req
@@ -54,7 +53,7 @@ file_name = ""
 @app.route('/index')
 def index():
     return render_template("index.html",
-       title = 'Home', user = { 'nickname': 'Kevin' }, api_key = google_maps_api_key,
+       title = 'Home', user = { 'nickname': 'Kevin' },
        )
 
 @app.route('/output_maps')
@@ -62,7 +61,7 @@ def output_maps():
 	#pull 'birth_month' from input field and store it
 	coords = request.args.get('coords')
 	print coords
-	return render_template("output_maps.html", api_key = google_maps_api_key)
+	return render_template("output_maps.html")
 
 @app.route('/about')
 def about():
@@ -83,53 +82,14 @@ def get_post_clickFindSolarPanels():
 
 	NE_coords = [float(request.form["NE_corner_lat"]), float(request.form["NE_corner_lng"])]
 	SW_coords = [float(request.form["SW_corner_lat"]), float(request.form["SW_corner_lng"])]
-	#if center_coords == []:
-	#	center_coords.append([float(request.form["center_lat"]), float(request.form["center_lng"])])
-	#else:
 	center_coords[0] = float(request.form["center_lat"])
 	center_coords[1] = float(request.form["center_lng"])
 	print "NE coords:",NE_coords
 	print "SW coords:",SW_coords
 	print "Center coords:",center_coords
 
-	#print request.form["hello"]
-	#print request.form["center_lat"]
-	#print request.form["center_lng"]
-	#print request.form["NE_corner_lat"]
-	#print request.form["SW_corner_lat"]
-
-	#Latitude difference across entire image
-	#delta_lat = NE_coords[0] - SW_coords[0]
-	#delta_lng = NE_coords[1] - SW_coords[1] 
-	#image_centers = [[center_coords[0] + delta_lat/4.0, center_coords[1] + delta_lng/4.0],
-	#				[center_coords[0] + delta_lat/4.0, center_coords[1] - delta_lng/4.0],
-	#				[center_coords[0] - delta_lat/4.0, center_coords[1] + delta_lng/4.0],
-	#				[center_coords[0] - delta_lat/4.0, center_coords[1] - delta_lng/4.0]]
-	#print image_centers
-
-	#for i in range(len(image_centers)):
-	#	img = acquire_image(*image_centers[i])
-	#	misc.imsave('test_img_'+str(i)+'.png', img)
-
 	print "Acquiring image"
-	img = acquire_image(center_coords[0], center_coords[1])
-	time.sleep(1)
-	file_name = str(uuid.uuid1())+".png"
-	print file_name
-	#misc.imsave('test_img.png', img)
-	print "Saving image"
-	misc.imsave(file_name, img)
-	return ""
-
-
-@app.route('/postmethod_clickShowSolarPanels', methods = ['POST'])
-def get_post_clickShowSolarPanels():
-	global file_name
-
-	print "Inside clickShowSolarPanels (Python)"
-	print "request.data:", request.data
-	print "request.args:", request.args
-	print "request.form:", request.form
+	big_img = acquire_image(center_coords[0], center_coords[1])
 
 	solar_locs = []
 	delta_lat = 0.000428567008235
@@ -141,70 +101,45 @@ def get_post_clickShowSolarPanels():
 	clf = joblib.load('RandomForest_classifier.pkl')
 	print clf
 
-	#file_list = listdir('.')
-	#print file_list
+	#threshold = 70
+	thresholds = [50,70,90,120]
+	for thresh in thresholds:
+		dbscan_results = dbscan_features_new(big_img, thresh)
 
-	#img_file_list = [fname for fname in file_list if '.png' in fname]
-	#img_file_list = ['test_img.png']
-	print file_name
-	img_file_list = [file_name]
-	print img_file_list
+		if len(dbscan_results) <= 0:
+			print "Skipping because len(dbscan_results)=", len(dbscan_results)
+			continue
 
-	#Go through all the files, split them and find their features, checking for any solar panels:
-	threshold = 70
-	thresholds = [50,70,90,120] #[60,70,80]
-	#thresholds = [70]
-	for img_index,img_file in enumerate(img_file_list):
-		print "Reading image, ",img_file
-		big_img = misc.imread(img_file)
-		#print type(img), img.dtype
-		#img_list = split_images(big_img)
-		#for tile_index,img in enumerate(img_list):
-		for thresh in thresholds:
-			dbscan_results = dbscan_features_new(big_img, thresh)
+		features = [res[0:9] for res in dbscan_results]
+		if len(features) == 1:
+			features = np.array(features).reshape(1, -1)
+		else:
+			features = np.array(features)
 
-			if len(dbscan_results) <= 0:
-				print "Skipping because len(dbscan_results)=", len(dbscan_results)
-				continue
-
-			features = [res[0:9] for res in dbscan_results]
-			if len(features) == 1:
-				features = np.array(features).reshape(1, -1)
-			else:
-				features = np.array(features)
-
-			print "After continue"
-			predictions = clf.predict(features)
-			solar_indices = np.where(predictions == 1)[0]
-			print "Classifier predictions: ",predictions
-			print "Success indices: ",solar_indices
-			if len(solar_indices) > 0:
-				for s_index in solar_indices:
-					#Define circles
-					x_cent = dbscan_results[s_index][9]
-					y_cent = dbscan_results[s_index][10]
-					#x_cluster = dbscan_results[j][9]
-					#y_cluster = dbscan_results[j][10]
-					#n_cluster = dbscan_results[j][11]
-					#print "Solar panels found in tile "+str(tile_index)+" of "+img_file+" thresh:"+str(thresh)
-					print "Solar panels found!"
-					print x_cent, y_cent
-					print center_lat, center_lng
-					solar_lat = (center_lat + delta_lat/2.0) - (y_cent/800.0)*delta_lat
-					solar_lng = (center_lng - delta_lng/2.0) + (x_cent/1280.0)*delta_lng
-					print solar_lat
-					print solar_lng
-
-					#solar_locs.append({"center_loc":[solar_lat,solar_lng]})
-					solar_locs.append([solar_lat,solar_lng])
-	
-	#return "Hi from Python!"
-	#return jsonify([0.0001, 0.0001])
-	#return jsonify({"center_loc":[36.9741385,-122.0317605]})
+		print "After continue"
+		predictions = clf.predict(features)
+		solar_indices = np.where(predictions == 1)[0]
+		print "Classifier predictions: ",predictions
+		print "Success indices: ",solar_indices
+		if len(solar_indices) > 0:
+			for s_index in solar_indices:
+				#Define circles
+				x_cent = dbscan_results[s_index][9]
+				y_cent = dbscan_results[s_index][10]
+				#x_cluster = dbscan_results[j][9]
+				#y_cluster = dbscan_results[j][10]
+				#n_cluster = dbscan_results[j][11]
+				#print "Solar panels found in tile "+str(tile_index)+" of "+img_file+" thresh:"+str(thresh)
+				print "Solar panels found!"
+				print x_cent, y_cent
+				print center_lat, center_lng
+				solar_lat = (center_lat + delta_lat/2.0) - (y_cent/800.0)*delta_lat
+				solar_lng = (center_lng - delta_lng/2.0) + (x_cent/1280.0)*delta_lng
+				print solar_lat
+				print solar_lng
+				solar_locs.append([solar_lat,solar_lng])
 
 	#Also make sure to delete file that we downloaded so that it doesn't pollute the server
-	subprocess.call(['rm', file_name])
+	#subprocess.call(['rm', file_name])
 
 	return jsonify({"data":solar_locs})
-
-
